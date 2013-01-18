1470e0aa8b282664d9554ccf19c3cac6ef28075a
Thang Pham <duythang.net@gmail.com>
2013-01-18 23:58:54 +0700

    Translate public/js/i18n

---
+++ public/js/i18n/vi.js
@@ -0,0 +1,18 @@
+{
+  "buttons": {
+    "run": "Chạy",
+    "clear": "Xoá hết",
+    "open": "Mở file",
+    "save": "Lưu",
+    "stop": "Dừng"
+  },
+  "tabs": {
+    "help-link": "Trợ giúp",
+    "output-link": "Kết quả",
+    "turtle-link": "Turtle"
+  },
+  "editor": {
+    "your-code": "Xin mời gõ mã lập trình vào phía dưới",
+    "confirm-clear": "Có chắc là bạn muốn xoá tất cả mã Ruby trên trình soạn thảo không?"
+  }
+}
